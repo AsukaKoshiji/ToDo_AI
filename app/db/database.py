a09662a83c35bbe067e1c9a49710b822af1d5180
asuka_koshiji <asuka_koshiji@asuka-koshijinoMacBook-Air.local>
2026-05-18 14:51:21 +0900

    deploy to render

--- app/db/database.py
+++ app/db/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "mysql+mysqlconnector://user:password@db/todo_db")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
